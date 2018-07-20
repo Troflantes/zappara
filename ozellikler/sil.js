2f0594efb37d2cd7fdc3e4b37adc42e77f148c1b
Troflantes <39567199+Troflantes@users.noreply.github.com>
2018-07-20 11:21:55 +0300

    Update sil.js

--- ozellikler/sil.js
+++ ozellikler/sil.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Hayır dostum!");
   if(!args[0]) return message.channel.send("Lütfen bir numara belirtin.");
   message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`**Sohbette ${args[0]} mesaj silindi.**\n**__Not:__**\n**__Bu Mesaj 30 Saniye Sonra Kendini İmha 🔫 Edicektir.**`).then(msg => msg.delete(3000));
+  message.channel.send(`**Sohbette ${args[0]} mesaj silindi.**\n**__Not:__**\n**__Bu Mesaj 10 Saniye Sonra Kendini İmha 🔫 Edicektir.**`).then(msg => msg.delete(2000));
 });
 
 }
